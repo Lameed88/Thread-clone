fa9cf87a5803fc64c593a95e3d70e06c7da6a484
Lameed88 <moradeyor88@gmail.com>
2023-11-29 10:43:50 +0100

    update

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -67,7 +67,8 @@ const loginUser = async (req, res) => {
         })
 
     } catch (error) {
-        res.status(500).json
+        res.status(500).json({error: error.message})
+        console.log("Error in ");
         
     }
 
