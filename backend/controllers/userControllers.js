4259282326490733a59f31f0f7c9268430e862a4
Lameed88 <moradeyor88@gmail.com>
2023-11-29 11:57:28 +0100

    update

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -91,6 +91,7 @@ const followUnFollowUser = async (req, res) => {
     const currentUser = await User.findById(id)(req.user_id)
 
     if (id === req.user._id.toString())
+    return res.status(400)
 
     try {
         
