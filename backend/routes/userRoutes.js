444e2924a5ab0a8b68ba24a1823cf965168f7bdd
Lameed88 <moradeyor88@gmail.com>
2023-11-29 11:46:09 +0100

    update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router()
 router.post("/signup", signUpUser)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser)
-router.post("/follow/:id", protectRoute, followUnFollowUser)
+router.post("/follow/:id", protectRoute, followUnFollowUser) // toggle state(follow/unfollow)
 
 module.exports = router
