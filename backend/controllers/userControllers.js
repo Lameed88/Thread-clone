95ed6aabb047849b76db0385ab9a0a6a0cc31170
Lameed88 <moradeyor88@gmail.com>
2023-11-29 10:30:57 +0100

    update

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -56,7 +56,13 @@ const loginUser = async (req, res) => {
 
         generateTokenAndSetCookie(user._id, res)
         res.status(200).json({
-            
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            username: user.username,
+            bio: user.bio,
+            profilePic
+            name: user.name,
         })
     } catch (error) {
         
