94e77fc2e186b387ebd71b5ddaf0d3b44ffe0fc5
Lameed88 <moradeyor88@gmail.com>
2023-11-29 10:19:35 +0100

    update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const { signUpUser, loginUser } = require ('../controllers/userControllers')
-
+ 
 const router = express.Router()
 
 router.post("/signup", signUpUser)
