a0ef8a56fec5884af5f3c803bf916dd449618da6
Lameed88 <moradeyor88@gmail.com>
2023-11-30 15:53:35 +0100

    update

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -31,6 +31,8 @@ const createPost = async (req, res) =>{
     }
 
 }
+
+
 const getPost = async(req, res) =>{
     try {
         const post = await Post.findById(req.params.id)
@@ -48,6 +50,8 @@ const getPost = async(req, res) =>{
 
 
 }
+
+
 
 const deletePost = async (req, res) =>{
     try {
@@ -69,4 +73,89 @@ const deletePost = async (req, res) =>{
     }
 }
 
-module.exports = { createPost, getPost, deletePost }
+
+
+const likeUnlikePost = async (req, res) =>{
+try {
+    const {id:postId } = req.params
+    const userId = req.user._id
+
+    const post = await Post.findById(postId)
+
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+
+    }
+
+    const userLikedPost = post.likes.includes(userId)
+
+    if (userLikedPost){
+        await Post.updateOne({_id:postId}, {$pull: {likes: userId}})
+        res.status(200).json({message: "Post unliked successfully"})
+
+    }else{
+        post.likes.push(userId)
+        await post.save()
+
+        res.status(200).json({message: "Post liked successfully"})
+    }
+} catch (error) {
+    res.status(500).json({message: error.message})// internal server error
+    console.log("Error in like Unlike Post : ", error.message);
+}
+}
+
+
+
+const replyToPost = async (req, res) =>{
+    try {
+        const{text} = req.body
+        const postId = req.params.id
+        const userId = req.user._id
+        const userProfilePic = req.user.ProfilePic
+        const username = req.user.username
+        
+        if(!text){
+            return res.status(400).json({message: "Text field is required"})
+        }
+        const post = await Post.findById(postId)
+
+        if(!post){
+            return res.status(404).json({message: "post not found"})
+        }
+
+        const reply = {userId, text, userProfilePic, username}
+
+        post.replies.push(reply)
+        await post.save()
+
+        res.status(200).json({message: "reply added successfully", post})
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})// internal server error
+        console.log("Error in Reply to Post : ", error.message);
+    }
+}
+
+
+const getFeedPost = async(req, res) => {
+
+    try {
+        const userId = req.user._id
+        const user = await User.findById(userId)
+
+        if (!user){
+            return res.status(404).json({message: "User not found"})
+        }
+        
+        const following = user.following
+
+
+
+    } catch (error) {
+        res.status(500).json({message: error.message})// internal server error
+        console.log("Error in Get feed Post : ", error.message);
+    }
+}
+
+module.exports = { createPost, getPost, deletePost, likeUnlikePost, replyToPost, getFeedPost }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,15 @@
 const express = require ('express')
-const { createPost, getPost, deletePost } = require ('../controllers/postControllers')
+const { createPost, getPost, deletePost, likeUnlikePost, replyToPost, getFeedPost } = require ('../controllers/postControllers')
 const protectRoute = require ('../middleware/protectRoute')
 
 const router = express.Router()
 
+router.post("/feed", protectRoute, getFeedPost )
 router.get("/:id", getPost)
 router.post("/create", protectRoute, createPost)
 router.delete("/:id", protectRoute, deletePost)
+router.post("/like/:id", protectRoute, likeUnlikePost)
+router.post("/reply/:id", protectRoute, replyToPost)
+
 
 module.exports = router
