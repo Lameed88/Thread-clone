3053f6c2d6cf0678b37bc9958447c662f9e7c982
Lameed88 <moradeyor88@gmail.com>
2023-12-04 09:13:55 +0100

    update

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -153,7 +153,7 @@ const getFeedPost = async(req, res) => {
         const feedPosts = await Post.find({postedBy: {$in: following}}).sort({createdAt: -1})
 
         res.status(200).json({feedPosts})
-
+ 
          
   
     } catch (error) {
