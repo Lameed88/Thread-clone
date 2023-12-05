494e88812878562291009ff8dcca8278fca905f9
Lameed88 <moradeyor88@gmail.com>
2023-12-05 09:12:52 +0100

    update

--- Frontend/src/components/LogoutButton.js
+++ Frontend/src/components/LogoutButton.js
@@ -1,11 +1,11 @@
 import { Button } from "@chakra-ui/react"
-import { FiLogout } from "react-icons/fi"
+import { FiLogOut } from "react-icons/fi"
 
 
 const LogoutButton = () => {
   return (
     <Button position={"fixed"} top={"30px"} right={"30px"} size={"sm"}>
-        <FiLogout size={20}/>
+        <FiLogOut size={20}/>
     </Button>
   )
 }
