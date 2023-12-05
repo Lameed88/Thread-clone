2e28303506950bd24491d7a9ed013fb824223ea3
Lameed88 <moradeyor88@gmail.com>
2023-12-05 12:13:09 +0100

    update

--- Frontend/src/pages/UpdateProfilePage.js
+++ Frontend/src/pages/UpdateProfilePage.js
@@ -37,20 +37,11 @@ export default function UpdateProfilePage() {
           User Profile Edit
         </Heading>
         <FormControl id="userName">
-          <FormLabel>User Icon</FormLabel>
+         
           <Stack direction={['column', 'row']} spacing={6}>
             <Center>
               <Avatar size="xl" src="https://bit.ly/sage-adebayo">
-                <AvatarBadge
-                  as={IconButton}
-                  size="sm"
-                  rounded="full"
-                  top="-10px"
-                  colorScheme="red"
-                  aria-label="remove Image"
-                  icon={<SmallCloseIcon />}
-                />
-              </Avatar>
+                
             </Center>
             <Center w="full">
               <Button w="full">Change Icon</Button>
