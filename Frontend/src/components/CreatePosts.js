1404d7c5e235b14498a15452b435f5bf675321e5
Lameed88 <moradeyor88@gmail.com>
2023-12-07 10:33:53 +0100

    update

--- Frontend/src/components/CreatePosts.js
+++ Frontend/src/components/CreatePosts.js
@@ -1,72 +1,104 @@
-import { AddIcon } from '@chakra-ui/icons'
-import { Button, FormControl, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea, useDisclosure, Text, Input, Flex } from '@chakra-ui/react'
-import { useRef, useState } from 'react'
-import usePreviewImg from '../hooks/usePreviewImg'
-import { BsFillImageFill } from 'react-icons/bs'  
-
-
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  FormControl,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+  useDisclosure,
+  Text,
+  Input,
+  Flex,
+  CloseButton,
+} from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import usePreviewImg from "../hooks/usePreviewImg";
+import { BsFillImageFill } from "react-icons/bs";
 
 const CreatePosts = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [postText, setPostText] = useState("");
+  const { handleImageChange, imgUrl } = usePreviewImg();
+  const imageRef = useRef(null);
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [postText, setPostText] = useState("")
-    const {handleImageChange, imgUrl} = usePreviewImg()
-    const imageRef= useRef(null)
- 
-    const handleTextChange = () => {
-
-    } 
+  const handleTextChange = () => {};
 
   return (
     <>
-    <Button position={"fixed"} bottom={10} right={10} leftIcon={<AddIcon />} bg={"gray"} onClick={onOpen}>
+      <Button
+        position={"fixed"}
+        bottom={10}
+        right={10}
+        leftIcon={<AddIcon />}
+        bg={"gray"}
+        onClick={onOpen}
+      >
         Post
-    </Button>
-    <Modal isOpen={isOpen} onClose={onClose}>
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Create Post</ModalHeader>
           <ModalCloseButton />
           <ModalBody p={6}>
             <FormControl>
-                <Textarea
-                placeholder='Post Content Goes Here'
+              <Textarea
+                placeholder="Post Content Goes Here"
                 onChange={handleTextChange}
-                value={ postText }
-                />
-                <Text fontSize= {"xs"} fw={"bold"} textAlign={"right"} m={1} color={"gray.800"}>
-                    500/500
-                </Text>
-                <Input type='file' hidden ref={imageRef} onChange={handleImageChange}/>
+                value={postText}
+              />
+              <Text
+                fontSize={"xs"}
+                fw={"bold"}
+                textAlign={"right"}
+                m={1}
+                color={"gray.800"}
+              >
+                500/500
+              </Text>
+              <Input
+                type="file"
+                hidden
+                ref={imageRef}
+                onChange={handleImageChange}
+              />
 
-                <BsFillImageFill
-                style={{marginLeft: "5px", cursor: "pointer"}}
+              <BsFillImageFill
+                style={{ marginLeft: "5px", cursor: "pointer" }}
                 size={16}
                 onClick={() => imageRef.current.click()}
-                />
+              />
             </FormControl>
 
             {imgUrl && (
-                <Flex mt={"full"} position={"relative"} >
-
-                <Image src= {imgUrl} alt="select img"/>
-
-                
-                </Flex>
-
+              <Flex mt={"full"} position={"relative"}>
+                <Image src={imgUrl} alt="select img" />
+                <CloseButton
+                  onClick={() => {
+                    setImgUrl("");
+                  }}
+                  bg={"gray.800"}
+                  position={"absolute"}
+                  top={2}
+                />
+              </Flex>
             )}
           </ModalBody>
- 
+
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
             </Button>
-          
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  )
-}
+  );
+};
 
-export default CreatePosts
+export default CreatePosts;
