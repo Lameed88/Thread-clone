8a7654446823985495d0497afa047593be86986c
Lameed88 <moradeyor88@gmail.com>
2023-12-07 09:16:35 +0100

    update

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -16,7 +16,7 @@ import CreatePosts from './components/CreatePosts';
 function App() {
   const user = useRecoilValue(userAtom) 
   console.log(user);
-  
+     
   return (
     <Container maxW="620px"> 
     <Header />
