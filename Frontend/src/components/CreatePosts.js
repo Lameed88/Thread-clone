0aa92c5b042e51d2ea4e2dc920cfde563692e3a0
Lameed88 <moradeyor88@gmail.com>
2023-12-07 10:15:08 +0100

    update

--- Frontend/src/components/CreatePosts.js
+++ Frontend/src/components/CreatePosts.js
@@ -36,10 +36,12 @@ const CreatePosts = () => {
                 <Text fontSize= {"xs"} fw={"bold"} textAlign={"right"} m={1} color={"gray.800"}>
                     500/500
                 </Text>
-                <input type='file' hidden ref={imageRef} onChange={handleImageChange}/>
+                <Input type='file' hidden ref={imageRef} onChange={handleImageChange}/>
 
                 <BsFillImageFill
                 style={{marginLeft: "5px", cursor: "pointer"}}
+                size={16}
+                onClick={()}
                 />
             </FormControl>
           </ModalBody>
