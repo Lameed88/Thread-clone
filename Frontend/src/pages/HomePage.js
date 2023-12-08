07d37e53169d672c9d972620d33de187ea0b738f
Lameed88 <moradeyor88@gmail.com>
2023-12-08 10:40:49 +0100

    update

--- Frontend/src/pages/HomePage.js
+++ Frontend/src/pages/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
   useEffect(() => {
     const getFeedPosts = async () =>{
       try {
-        const res = await fetch("/api/posts/fed")
+        const res = await fetch("/api/posts/feed")
         const data = await res.json()
         console.log(data);
         if(data.error) {
