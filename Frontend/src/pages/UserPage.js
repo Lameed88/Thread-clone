bc96fa470a60979d94b60df23b1ff934c44b7061
Lameed88 <moradeyor88@gmail.com>
2023-12-08 09:13:29 +0100

    update

--- Frontend/src/pages/UserPage.js
+++ Frontend/src/pages/UserPage.js
@@ -3,6 +3,7 @@ import UserHeader from "../components/UserHeader";
 import UserPosts from "../components/UserPosts";
 import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 const UserPage = () => {
   const [user, setUser] = useState(null);
