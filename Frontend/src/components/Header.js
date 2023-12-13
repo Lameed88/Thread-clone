046ac9e7623038ed89a7c8e9140fedf6a7e6190c
Lameed88 <moradeyor88@gmail.com>
2023-12-13 10:29:54 +0100

    update

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,56 +1,60 @@
-import { Button, Flex, Image, useColorMode } from "@chakra-ui/react";
-import { useRecoilValue } from "recoil";
+import { Flex, Image, useColorMode, Link, Button } from "@chakra-ui/react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
-import { AiFillHome } from "react-icons/ai"
-import {Link, Link as RouterLink} from "react-router-dom"
-import { useState } from "react";
-import {RxAvatar} from "react-icons/rx"
-import {FiLogOut} from "react-icons/fi"
+import { AiFillHome, AiOutlineLogout } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
+import { FiLogOut } from "react-icons/fi";
+import { Link as RouterLink } from "react-router-dom";
+import useLogOut from "../hooks/useLogOut.js";
+import authScreenAtom from "../atoms/authAtom.js";
+// import RxAvatar from "./RxAvatar";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const user = useRecoilValue(userAtom)
-  const [authScreen, setAuthScreen] = useState(false)
+  const user = useRecoilValue(userAtom);
+
+  const logout = useLogOut();
+  const setAuthScreen = useSetRecoilState(authScreenAtom)
 
   return (
     <Flex justifyContent={"space-between"} mt={6} mb="12">
       {user && (
         <Link as={RouterLink} to="/">
-          <AiFillHome size={24} />
+          <AiFillHome fontSize={24} />
         </Link>
       )}
 
-      {/* {!user && (
-        <Link as= {RouterLink} to={"/auth"} onClick={() => setAuthScreen("login")}>
-          login
+      {!user && (
+        <Link as={RouterLink} to= '/auth' onClick={() => setAuthScreen("login")}>
+          Login
         </Link>
-      )} */}
+      )}
 
       <Image
+        cursor="pointer"
         alt="logo"
         w={6}
         src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
         onClick={toggleColorMode}
-        cursor={"pointer"}
       />
 
       {user && (
         <Flex alignItems={"center"} gap={4}>
-
-        <Link as={RouterLink} to={`/${user.username}`}>
-          <RxAvatar size={24}/>
-        </Link>
-
-        <Button size={"xs"} >
-        <FiLogOut size={20}/>
-    </Button>
-
+          <Link as={RouterLink} to={`/${user.username}`}>
+            <RxAvatar fontSize={24} />
+          </Link>
+          <Button size={"xs"} onClick={logout}>
+            <FiLogOut size={20} />
+          </Button>
         </Flex>
-
       )}
- 
+      {!user && (
+        <Link as={RouterLink} to= "/auth" onClick={() => setAuthScreen("signUp")}>
+          Sign Up
+        </Link>
+      )}
     </Flex>
   );
 };
-  
+
 export default Header;
--- Frontend/src/hooks/useLogOut.js
+++ Frontend/src/hooks/useLogOut.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useRecoilState, useSetRecoilState } from "recoil"
+import userAtom from "../atoms/userAtom"
+import useShowToast from "./useShowToast"
 
 const useLogOut = () => {
-  return (
-    <div>useLogOut</div>
-  )
-}
+    const setUser = useSetRecoilState(userAtom)
+    const showToast =useShowToast()
 
+    const logout = async () => {
+        try {
+          const res = await fetch('/api/users/logout', {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json',
+            }
+          })
+
+          const data = await res.json()
+          
+          if (data.error) {
+            showToast("Error", data.error, 'error');
+            return;
+          }
+          localStorage.removeItem("user-threads");
+          setUser(null)
+    
+        } catch (error) {
+          showToast("Error", error, "error")
+        }
+    }
+
+    return logout
+}
 export default useLogOut
